f974913cfe802af707e4739387878fc7768c3ad5
anmolgautam <anmol.gautam@neocard.fi>
2019-03-03 02:22:14 +0200

    adding python code

---
+++ bin/challenge.py
@@ -0,0 +1,11 @@
+import time, sys
+start_time = time.time()
+anagramList = []
+with open('lemmad.txt', 'rb') as file:
+    for value in enumerate(file):
+        w = value[1].strip('\r\n')
+        if(sorted(w) == sorted(sys.argv[1])):
+            anagramList.append(w)
+print('Output (duration,list,of,anagrams,found): ' +
+ str((time.time() - start_time) * 1000000) +
+ ',' + ",".join(map(str,anagramList)))
